912464bb8929ae6e71828fdd8bea50745c332f3f
Sufaina-Nesrin <sufainanasrin2003@gmail.com>
2023-04-09 16:29:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [place, setPlace] = useState("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API_KEY}`
+      )
+      .then((response) => {
+        setSearch(response.data.main);
+        console.log(response.data.main);
+      })
+      .catch((err) => {
+        setSearch("");
+      });
+  }, [place]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <div className="App-header">
+          <h1 className="title">Weather-App🌥️</h1>
+          <div className="input-box-div">
+            <input
+              type="search"
+              placeholder="Enter the Place"
+              className="inputField"
+              onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            />
+            <button className="addButton">
+              <i class="fa-solid fa-location-dot"></i>
+            </button>
+          </div>
+          <div>
+            <h2 className="info">{place}</h2>
+
+            {search.temp > 0 && place !== "" ? (
+              <idv>
+                <h3 className="info-2">
+                  {(search.temp - 273.15).toFixed(1)}
+                  {"\u00b0C"}
+                </h3>
+                <h6 className="info-3">
+                  min-temp : {search.temp_min} | max-temp : {search.temp_max}
+                </h6>
+              </idv>
+            ) : (
+              <h3 className="info-2">Not Found</h3>
+            )}
+            {console.log(place)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
